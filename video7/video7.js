d06432cf5866f25d10435bb49b0ccb3d1d9b9025
dinhvandang <dinhvandang.devo@gmail.com>
2022-03-21 15:50:44 +0700

    template string (user `${}`

---
+++ video7/video7.js
@@ -0,0 +1,14 @@
+    console.log(hello world from js)
+
+    let a = 5;
+    let b = 10;
+    console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+    // "Fifteen is 15 and
+    // not 20."
+
+    let a = 5;
+    let b = 10;
+    console.log(`Fifteen is ${a + b} and
+not ${2 * a + b}.`);
+    // "Fifteen is 15 and
+    // not 20."
