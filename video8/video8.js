693594e4ed24f3a20a8d2c9969601934746a263e
dinhvandang <dinhvandang.devo@gmail.com>
2022-03-21 19:57:42 +0700

    object

---
+++ video8/video8.js
@@ -0,0 +1,11 @@
+// object ( key: value , key:  value) 
+
+let obj = {
+    name: 'Dang',
+    address: 'HCM',
+    email: '',
+    gender: ''
+}
+
+console.log('My name is: ', obj.name) //co the su dung obj['name']
+console.log('My address is: ', obj.address) //co the su dung obj['address'] va tuong tu cac key khac
